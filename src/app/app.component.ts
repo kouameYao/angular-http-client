35f3e1145d15519e3f9539f977c689bad1b9107a
kouameYao <yaojean0857@gmail.com>
2021-10-11 08:53:40 +0000

    First commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,47 @@
+import { Component } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'angular-http';
+
+  apiRoot: string = "http://httpbin.org";
+
+  constructor(private http: HttpClient) { }
+
+  doGET() {
+    console.log("GET");
+  }
+
+  doPOST() {
+    console.log("POST");
+  }
+
+  doPUT() {
+    console.log("PUT");
+  }
+
+  doDELETE() {
+    console.log("DELETE");
+  }
+
+  doGETAsPromise() {
+    console.log("GET AS PROMISE");
+  }
+
+  doGETAsPromiseError() {
+    console.log("GET AS PROMISE ERROR");
+  }
+
+  doGETAsObservableError() {
+    console.log("GET AS OBSERVABLE ERROR");
+  }
+
+  doGETWithHeaders() {
+    console.log("GET WITH HEADERS");
+  }
+
+}
