0d583160a7985f9a05a0df78dff7763fddbf6004
kouameYao <yaojean0857@gmail.com>
2021-10-11 17:46:24 +0000

    And of http module end

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,47 +1,103 @@
-import { Component } from '@angular/core';
-import { HttpClient } from "@angular/common/http";
+import { Component } from "@angular/core";
+import {
+  HttpClient,
+  HttpHeaders,
+  HttpParams
+} from "@angular/common/http";
+//import { toPromise } from "rxjs/operators";
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'angular-http';
+  apiRoot: string = "https://httpbin.org";
 
-  apiRoot: string = "http://httpbin.org";
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   doGET() {
     console.log("GET");
+    let url = `${this.apiRoot}/get`;
+    const httpOptions = {
+      params: new HttpParams().set("foo", "moo").set("limit", "25")
+    };
+    this.http.get(url, httpOptions).subscribe(res => console.log(res));
   }
 
   doPOST() {
     console.log("POST");
+    let url = `${this.apiRoot}/post`;
+    const httpOptions = {
+      params: new HttpParams().set("foo", "moo").set("limit", "25")
+    };
+    this.http.post(url, { moo: "foo", goo: "loo" }, httpOptions).subscribe(res => console.log(res));
   }
 
   doPUT() {
     console.log("PUT");
+    let url = `${this.apiRoot}/put`;
+    const httpOptions = {
+      params: new HttpParams().set("foo", "moo").set("limit", "25")
+    };
+    this.http.put(url, { moo: "foo", goo: "loo" }, httpOptions).subscribe(res => console.log(res));
   }
 
   doDELETE() {
     console.log("DELETE");
+    let url = `${this.apiRoot}/delete`;
+    const httpOptions = {
+      params: new HttpParams().set("foo", "moo").set("limit", "25")
+    };
+    this.http.delete(url, httpOptions).subscribe(res => console.log(res));
   }
 
   doGETAsPromise() {
     console.log("GET AS PROMISE");
+    let url = `${this.apiRoot}/get`;
+    this.http
+      .get(url)
+      .toPromise()
+      .then(res => console.log(res));
   }
 
   doGETAsPromiseError() {
     console.log("GET AS PROMISE ERROR");
+    let url = `${this.apiRoot}/post`;
+    this.http
+      .get(url)
+      .toPromise()
+      .then(
+        res => console.log(res),
+        msg => console.error(`Error: ${msg.status} ${msg.statusText}`)
+      );
   }
 
+  // To catch an error, we call the get method with wrong resource : apiRoot
   doGETAsObservableError() {
     console.log("GET AS OBSERVABLE ERROR");
+    let url = `${this.apiRoot}/post`;
+    this.http.get(url).subscribe(
+        res => console.log(res),
+        msg => console.error(`Error: ${msg.status} ${msg.statusText}`)
+      );
   }
 
   doGETWithHeaders() {
     console.log("GET WITH HEADERS");
-  }
 
+    let url = `${this.apiRoot}/get`;
+
+    const httpOptions = {
+      headers: new HttpHeaders({
+        Authorization: btoa("username:password")
+      })
+    };
+
+    this.http.get(url, httpOptions).subscribe(
+        res => console.log(res),
+        msg => console.error(`Error: ${msg.status} ${msg.statusText}`)
+      );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClient } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +10,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    HttpClient,
+    HttpClientModule,
     BrowserModule,
     AppRoutingModule,
   ],
